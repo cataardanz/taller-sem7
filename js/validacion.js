34e48c5d702da63aeff0f321bc40b4a0a3c6c269
cataardanz <cataardanz@gmail.com>
2024-08-14 14:27:38 -0300

    dom y lets

--- js/validacion.js
+++ js/validacion.js
@@ -1,6 +1,16 @@
+document.getElementById ('registroForm').addEventListener('sumbit', function (event) {
+    let name = document.getElementById ('nombre').value;
+    let email = document.getElementById ('email').value;
+    let password = document.getElementById ('contrasena').value;
+    let repeatpassword = document.getElementById('repetirContrasena').value;
+    let termsacept = document.getElementById ('aceptoterminos').checked;
+})
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
+
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
