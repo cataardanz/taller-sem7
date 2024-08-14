5074977a89fc52fba5f7bfe31e8b94fabbf3d2ae
Valentina <perezvalentina0206@gmail.com>
2024-08-14 14:13:48 -0300

    Coincidencia de contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,8 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+if (contrasena !== repetirContrasena) {
+    showAlertError("Las contraseñas no coinciden.");
+    return;
+}
