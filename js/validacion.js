5d0cee128b5ee00280aaa8514d4c87c796a8f39c
aprilasmyname <sofiaavrilsaldivia@outlook.com>
2024-08-14 15:21:03 -0300

    validacion del checkbox y exito.

--- js/validacion.js
+++ js/validacion.js
@@ -4,4 +4,11 @@ function showAlertSuccess() {
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
-}}
+}
+if (!aceptoTerminos){
+    showAlertError("Debe aceptar los terminos y condiciones.");
+    return;
+}
+
+//si pasa todas las validaciones
+showAlertSuccess("Datos guardados correctamente.");
